c14a9de9e78c19d765a33a1f2d26bf2ab7960f0c
Allen <allenshamrock37@gmail.com>
2024-04-02 18:06:17 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -5,7 +5,6 @@ from flask import Flask, make_response
 from flask_migrate import Migrate
 
 from models import db, Earthquake
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -14,14 +13,38 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
-
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquake_by_id(id):
+    earthquake =Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        response_body = earthquake.to_dict()
+        status = 200
+    else:
+        response_body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    
+    return make_response(response_body,status)
+
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def earthquake_by_magnitude(magnitude):
+    quakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >=magnitude).all():
+        quakes.append(earthquake.to_dict())
+    response_body = {'count': len(quakes),
+                     'quakes':quakes
+                     }
+    status = 200
+
+    return make_response(response_body, status)
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
